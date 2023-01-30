e1ad75002c96607fc92d8bfd21d197a4d50aaf95
Jose Ramos <engrjvramos@gmail.com>
2023-01-31 04:47:25 +0800

    created homepage UI and movie rows component

---
+++ components/Header.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { MagnifyingGlassIcon, BellIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex ">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Tv Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden sm:inline w-6 h-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+            alt=""
+            className="cursor-pointer rounded h-10 w-10"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
